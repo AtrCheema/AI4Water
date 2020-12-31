05e0488b7d0e0a410cf17c20ef78b6170b059179
AtrCheema <ather_abbas786@yahoo.com>
2021-01-01 04:56:03 +0900

    remvoed a bug to clear test

--- tests/test_without_eager.py
+++ tests/test_without_eager.py
@@ -6,7 +6,6 @@ tf.compat.v1.disable_eager_execution()
 import site  # so that dl4seq directory is in path
 site.addsitedir(os.path.dirname(os.path.dirname(__file__)))
 
-from dl4seq.utils import make_model
 from dl4seq import InputAttentionModel, DualAttentionModel
 import unittest
 
@@ -16,24 +15,22 @@ from os.path import abspath
 
 def make_and_run(input_model, _layers=None, lookback=12, epochs=1, **kwargs):
 
-    config = make_model(batch_size=64,
-                        lookback=lookback,
-                        lr=0.001,
-                        epochs=epochs,
-                        **kwargs)
-
     file_path = abspath(getsourcefile(lambda: 0))
     dpath = os.path.join(os.path.join(os.path.dirname(os.path.dirname(file_path)), "dl4seq"), "data")
     fname = os.path.join(dpath, "nasdaq100_padding.csv")
     df = pd.read_csv(fname)
 
     model = input_model(
-        config=config,
         data=df,
-        verbosity=0
+        verbosity=0,
+        batch_size=64,
+        lookback=lookback,
+        lr=0.001,
+        epochs=epochs,
+        **kwargs
     )
 
-    _ = model.train(indices='random')
+    _ = model.fit(indices='random')
 
     _, pred_y = model.predict(use_datetime_index=False)
 
