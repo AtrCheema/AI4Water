9d7e33b20603d17f448c376fbd0c3b6cb8c98778
AtrCheema <ather_abbas786@yahoo.com>
2021-04-27 16:11:01 +0900

    updated

--- dl4seq/utils/datasets/read_netcdf.py
+++ dl4seq/utils/datasets/read_netcdf.py
@@ -2,14 +2,14 @@ import os
 import netCDF4
 
 
-for _f in os.listdir('datasets\\HYSETS'):
+for _f in os.listdir('data\\HYSETS'):
 
     if _f.endswith('.nc'):
 
-        nc = netCDF4.Dataset(os.path.join(os.getcwd(), f'datasets\\HYSETS\\{_f}'))
+        nc = netCDF4.Dataset(os.path.join(os.getcwd(), f'data\\HYSETS\\{_f}'))
 
         print(f'\nreading file {_f} ')
         for var in nc.variables:
-            print(var, nc[var].shape)
+            print('{0:30} {1}'.format(var, nc[var].shape))
 
         nc.close()
