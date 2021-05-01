94c64d2ae650e67717a73b801ee46b5029107e6a
AtrCheema <ather_abbas786@yahoo.com>
2021-05-02 00:23:51 +0900

    added some example datasets

--- AI4Water/utils/datasets/__init__.py
+++ AI4Water/utils/datasets/__init__.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+
 
 from AI4Water.utils.datasets.datasets import CAMELS_AUS
 from AI4Water.utils.datasets.datasets import CAMELS_CL
@@ -30,3 +33,16 @@ from AI4Water.utils.datasets.datasets import HydrometricParana
 from AI4Water.utils.datasets.datasets import RiverTempSpain
 from AI4Water.utils.datasets.datasets import RiverIsotope
 from AI4Water.utils.datasets.datasets import EtpPcpSamoylov
+
+
+def load_30min():
+    fpath = os.path.join(os.path.dirname(__file__), "data_30min.csv")
+    df = pd.read_csv(fpath, index_col="Date_Time2")
+    df.index=pd.to_datetime(df.index)
+    return df
+
+def load_u1():
+    """loads 1d data that can be used fo regression and classification"""
+    fpath = os.path.join(os.path.dirname(__file__), "input_target_u1.csv")
+    df = pd.read_csv(fpath)
+    return df
