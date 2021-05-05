f6ac4468da61340a7208ed9d73064d3587ee8e15
Sara Iftikhar <sara.rwpk@gmail.com>
2021-05-05 14:51:54 +0900

    mlp example working now

--- examples/mlp.py
+++ examples/mlp.py
@@ -1,11 +1,10 @@
 # this file shows how to build a simple dense layer based model
 # the input_features and outputs are columns and are present in the file
-import os
-
 import pandas as pd
 import tensorflow as tf
 
 from AI4Water import Model
+from AI4Water.utils.datasets import load_30min
 
 tf.compat.v1.disable_eager_execution()
 
@@ -20,9 +19,7 @@ input_features = ['input1', 'input2', 'input3', 'input4', 'input5', 'input6', 'i
 # column in dataframe to bse used as output/target
 outputs = ['target7']
 
-fname = os.path.join(os.path.dirname(os.path.dirname(__file__)), "AI4Water/data/data_30min.csv")
-df = pd.read_csv(fname)
-df.index = pd.to_datetime(df['Date_Time2'])
+df = load_30min()
 
 model = Model(data=df,
               batch_size=16,
