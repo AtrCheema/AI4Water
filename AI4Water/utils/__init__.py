8292d1a34b1054afe6805dfc4a42ec0211376125
Ather Abbas <ather_abbas786@yahoo.com>
2021-08-03 19:11:17 +0900

    Delete __init__.py

--- AI4Water/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from ai4water.utils.utils import make_model
-from ai4water.utils.transformations import Transformations
-from ai4water.utils.visualizations import Visualizations
-
-from ai4water.utils.taylor_diagram import taylor_plot
