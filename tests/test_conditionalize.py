75dfd3a456dc71617759d542cd3441aa9b7ba743
AtrCheema <ather_abbas786@yahoo.com>
2021-09-29 22:49:05 +0900

    added tests for conditional layer

---
+++ tests/test_conditionalize.py
@@ -0,0 +1,51 @@
+import unittest
+
+from tensorflow.keras.layers import Input, Dense, LSTM
+from tensorflow.keras.models import Model
+from ai4water.private_layers import Conditionalize
+from ai4water.datasets import arg_beach
+
+
+
+class TestConditionalize(unittest.TestCase):
+
+    def test_multiple_conds(self):
+        i = Input(shape=(10, 3))
+        h = Input(shape=(14,))
+        c = Conditionalize(32)([h, h, h])
+        rnn = LSTM(32)(i, initial_state=[c,c])
+        o = Dense(1)(rnn)
+
+        model = Model(inputs=[i, h], outputs=o)
+        model.compile('adam', loss='mse')
+        self.assertEqual(model.count_params(), 6085)
+
+    def test_single_cond(self):
+        i = Input(shape=(10, 3))
+        h = Input(shape=(14,))
+        c = Conditionalize(32)(h)
+        rnn = LSTM(32)(i, initial_state=[c, c])
+        o = Dense(1)(rnn)
+
+        model = Model(inputs=[i, h], outputs=o)
+        model.compile('adam', loss='mse')
+        self.assertEqual(model.count_params(), 5121)
+
+    def todo_test_ai4water_model(self):
+        #
+        # model = Model(model={"layers":
+        #                          {"Input": {"shape": (10, 3)},
+        #                           "Conditionalize": {"units": 32, "name": "h_state"},
+        #                           "LSTM": {"config": {"units": 32},
+        #                                    "inputs": ["Input"],
+        #                                    'call_args': {'initial_state': ['h_state', 'h_state']}
+        #                                    },
+        #                           "Dense": {"units": 1}
+        #                           }},
+        #               data=arg_beach()
+        #               )
+        return
+
+if __name__ == "__main__":
+
+    unittest.main()
