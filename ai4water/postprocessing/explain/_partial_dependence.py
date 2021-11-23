7580b66839302b458f950565cf9121f0bad8e335
Sara Iftikhar <sara.rwpk@gmail.com>
2021-11-24 01:22:26 +0900

    added tests for pdp plots

--- ai4water/postprocessing/explain/_partial_dependence.py
+++ ai4water/postprocessing/explain/_partial_dependence.py
@@ -58,7 +58,6 @@ class PartialDependencePlot(ExplainerMixin):
                 path to save the plots. By default the results are saved in current directory
             kwargs:
                 any additional keyword arguments for `model`
-
         """
 
         self.model = model
@@ -69,6 +68,203 @@ class PartialDependencePlot(ExplainerMixin):
 
         super().__init__(data=data, features=feature_names, path=path or os.getcwd())
 
+    def nd_interactions(
+            self,
+            height: int = 2,
+            ice: bool = False,
+            show_dist: bool = False,
+            show_minima: bool = False,
+            show: bool = True,
+            save: bool = False
+    ) -> plt.Figure:
+        """Plots 2d interaction plots of all features as done in skopt
+
+        Arguments:
+            height:
+                height of each subplot in inches
+            ice:
+                whether to show the ice lines or not
+            show_dist:
+                whether to show the distribution of data as histogram or not
+            show_minima:
+                whether to show the function minima or not
+            show:
+                whether to show the plot or not
+            save:
+                whether to save the plot or not
+        Returns:
+            matplotlib Figure
+        """
+
+        n_dims = len(self.features)
+
+        fig, ax = plt.subplots(n_dims, n_dims, figsize=(height * n_dims, height * n_dims))
+
+        fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95,
+                            hspace=0.1, wspace=0.1)
+
+        for i in range(n_dims):
+            for j in range(n_dims):
+                # diagonal
+                if i == j:
+
+                    if n_dims > 1:
+                        ax_ = ax[i, i]
+                    else:
+                        ax_ = ax
+
+                    self._plot_pdp_1dim(*self._pdp_for_2d(self.data, self.features[i]),
+                                        self.data, self.features[i],
+                                        show_dist=show_dist, show_minima=show_minima,
+                                        ice=ice, show=False, ax=ax_)
+                    # resetting the label
+                    #ax_.set_xlabel(self.features[i])
+                    ax_.set_ylabel(self.features[i])
+                    process_axis(ax_, xlabel=self.features[i], top_spine=True, right_spine=True)
+
+                # lower triangle
+                elif i > j:
+                    self.plot_interaction(
+                        features=[self.features[j], self.features[i]],
+                        show=False, ax=ax[i, j], colorbar=False
+                    )
+
+                elif j>i:
+                    if not ax[i,j].lines:  # empty axes
+                        ax[i,j].axis("off")
+
+                if j>0: # not the left most column
+                    ax[i, j].yaxis.set_ticks([])
+                    ax[i,j].yaxis.set_visible(False)
+                    ax[i, j].yaxis.label.set_visible(False)
+
+                if i<n_dims-1:  # not the bottom most row
+                    ax[i, j].xaxis.set_ticks([])
+                    ax[i, j].xaxis.set_visible(False)
+                    ax[i,j].xaxis.label.set_visible(False)
+
+        if save:
+            fname = os.path.join(self.path, f"pdp_interact_nd")
+            plt.savefig(fname, bbox_inches="tight", dpi=100*n_dims)
+
+        if show:
+            plt.show()
+
+        return fig
+
+    def plot_interaction(
+            self,
+            features: list,
+            lookback: int = None,
+            ax: plt.Axes = None,
+            plot_type:str = "2d",
+            cmap = None,
+            colorbar: bool = True,
+            show: bool = True,
+            save: bool = False,
+            **kwargs
+    ) -> plt.Axes:
+        """Shows interaction between two features
+
+        Arguments:
+            features:
+                a list or tuple of two feature names to use
+            lookback:
+
+            ax:
+                matplotlib axes on which to draw. If not given, current axes will
+                be used.
+            plot_type:
+                either "2d" or "surface"
+            cmap:
+                color map to use
+            colorbar:
+                whether to show the colorbar or not
+            show:
+                whether to show the plot or not
+            save:
+                whether to save the plot or not
+            kwargs:
+                any keyword argument for axes.plot_surface or axes.contourf
+
+        Returns:
+              matplotlib Axes
+        """
+
+        if not self.data_is_2d:
+            raise NotImplementedError
+
+        assert isinstance(features, list) and len(features) == 2
+
+        x0, x1, pd_vals = self._interaction(features, self.data, lookback)
+
+        kwds = {}
+        if plot_type == "surface":
+            kwds['projection'] = '3d'
+
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, **kwds)
+
+        if plot_type == "surface":
+            _add_surface(ax, x0, x1, pd_vals, cmap, features[0], features[1], **kwargs)
+        else:
+            self._plot_interaction(ax, x0, x1, pd_vals,
+                                   cmap=cmap,
+                                   features=features,
+                                   lookback=lookback,
+                                   colorbar=colorbar, **kwargs)
+
+        if save:
+            fname = os.path.join(self.path, f"pdp_interact{features[0]}_{features[1]}")
+            plt.savefig(fname, bbox_inches="tight", dpi=300)
+
+        if show:
+            plt.show()
+
+        return ax
+
+    def _plot_interaction(self, ax, x0, x1, pd_vals, cmap, features, lookback,
+                          colorbar=True,
+                          **kwargs):
+        """adds a 2d interaction plot"""
+        cntr = ax.contourf(x0, x1, pd_vals, cmap=cmap, **kwargs)
+
+        xv0 = self.xv(self.data, features[0], lookback)
+        xv1 = self.xv(self.data, features[1], lookback)
+
+        ax.scatter(xv0, xv1, c='k', s=10, lw=0.)
+        process_axis(ax, xlabel=features[0], ylabel=features[1])
+
+        if colorbar:
+            cbar = plt.colorbar(cntr, ax=ax)
+            cbar.set_label(f"E[f(x) | {features[0]} {features[1]} ]", rotation=90)
+
+        return
+
+    def _interaction(self, features, data, lookback):
+
+        ind0 = self._feature_to_ind(features[0])
+        ind1 = self._feature_to_ind(features[1])
+
+        xs0 = self.xs(self.data, features[0], lookback)
+        xs1 = self.xs(self.data, features[1], lookback)
+
+        features_tmp = data.copy()
+        x0 = np.zeros((self.num_points, self.num_points))
+        x1 = np.zeros((self.num_points, self.num_points))
+        pd_vals = np.zeros((self.num_points, self.num_points))
+
+        for i in range(self.num_points):
+            for j in range(self.num_points):
+                features_tmp[:, ind0] = xs0[i]
+                features_tmp[:, ind1] = xs1[j]
+                x0[i, j] = xs0[i]
+                x1[i, j] = xs1[j]
+                pd_vals[i, j] = self.model(features_tmp).mean()
+
+        return x0, x1, pd_vals
+
     def plot_1d(
             self,
             feature,
@@ -78,6 +274,7 @@ class PartialDependencePlot(ExplainerMixin):
             feature_expected_value: bool = False,
             model_expected_value: bool = False,
             show_ci: bool = False,
+            show_minima: bool = False,
             show:bool = True,
             save: bool = False
     ):
@@ -98,6 +295,8 @@ class PartialDependencePlot(ExplainerMixin):
                 whether to show average prediction on plot or not
              show_ci:
                 whether to show confidence interval of pdp or not
+            show_minima:
+                whether to indicate the minima or not
             show:
                 whether to show the plot or not
             save:
@@ -111,13 +310,13 @@ class PartialDependencePlot(ExplainerMixin):
                     self._plot_pdp_1dim(*self._pdp_for_2d(self.data, feature), self.data, feature,
                                         show_dist=show_dist, show_dist_as=show_dist_as,
                                         ice=ice, feature_expected_value=feature_expected_value,
-                                        show_ci=show_ci,
+                                        show_ci=show_ci, show_minima=show_minima,
                                         model_expected_value=model_expected_value, show=show, save=save)
                 elif self.data_is_3d:
                     for lb in range(self.data.shape[1]):
                         self._plot_pdp_1dim(*self._pdp_for_2d(self.data, feature, lb),
                                             data=self.data, feature=feature, lookback=lb,
-                                            show_ci=show_ci,
+                                            show_ci=show_ci, show_minima=show_minima,
                                             show_dist=show_dist, show_dist_as=show_dist_as,
                                             ice=ice, feature_expected_value=feature_expected_value,
                                             model_expected_value=model_expected_value, show=show, save=save)
@@ -131,11 +330,13 @@ class PartialDependencePlot(ExplainerMixin):
         return
 
     def xv(self, data, feature, lookback):
+
         ind = self._feature_to_ind(feature)
         if data.ndim==3:
             xv = data[:, lookback, ind]
         else:
             xv = data[:, ind]
+
         return xv
 
     def xs(self, data, feature, lookback=None):
@@ -180,13 +381,15 @@ class PartialDependencePlot(ExplainerMixin):
     def _plot_pdp_1dim(self,
                        pd_vals, ice_vals, data, feature, lookback=None,
                        show_dist=True, show_dist_as="hist", ice=True, show_ci=False,
+                       show_minima=False,
                        feature_expected_value=False, model_expected_value=False,
-                       show=True, save=False):
+                       show=True, save=False, ax=None):
 
         xmin, xmax = compute_bounds(self.xmin, self.xmax, self.xv(data, feature, lookback))
 
-        fig = plt.figure()
-        ax = fig.add_axes((0.1,0.3,0.8,0.6))
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_axes((0.1,0.3,0.8,0.6))
 
         xs = self.xs(data, feature, lookback)
 
@@ -235,6 +438,10 @@ class PartialDependencePlot(ExplainerMixin):
 
         if model_expected_value:
             self._add_model_exp_val(ax2, ax, data)
+
+        if show_minima:
+            minina = self.model(data, **self.kwargs).min()
+            ax.axvline(minina, linestyle="--", color="r", lw=1)
 
         if show:
             plt.show()
@@ -325,9 +532,13 @@ def process_axis(
 
     if top_spine is False:
         ax.spines['top'].set_visible(False)
+    elif top_spine is True:
+        ax.spines['top'].set_visible(True)
 
     if right_spine is False:
         ax.spines['right'].set_visible(False)
+    elif right_spine is True:
+        ax.spines['right'].set_visible(True)
 
     if bottom_spine is False:
         ax.spines['bottom'].set_visible(False)
@@ -351,3 +562,13 @@ def _add_dist_as_grid(fig:plt.Figure, hist_data, other_axes:plt.Axes,
     ax.plot(hist_data, [1] * len(hist_data), '|', color=color, markersize=20)
     return
 
+
+def _add_surface(ax, x0, x1, pd_vals, cmap, feature0, feature2, **kwargs):
+
+    ax.plot_surface(x0, x1, pd_vals, cmap=cmap, **kwargs)
+
+    ax.set_xlabel(feature0, fontsize=11)
+    ax.set_ylabel(feature2, fontsize=11)
+    ax.set_zlabel(f"E[f(x) | {feature0} {feature2} ]", fontsize=11)
+
+    return
