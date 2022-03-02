7cb4b96109cfd559dc8f5aa9446ec31c1e1dbc58
AtrCheema <ather_abbas786@yahoo.com>
2022-03-02 16:18:06 +0900

    modified beach_eda exmaple script

--- examples/eda_busan_beach_example.py
+++ examples/eda_busan_beach_example.py
@@ -1,7 +1,7 @@
-""
-===================
+""""
+=============
 eda_arg_beach
-==================
+=============
 """
 
 from ai4water.datasets import busan_beach
@@ -9,6 +9,8 @@ from ai4water.eda import EDA
 
 data = busan_beach(target=['ecoli', 'sul1_coppml', 'aac_coppml', 'tetx_coppml', 'blaTEM_coppml'])
 data.shape
+
+###########################################################
 
 eda = EDA(data)
 
