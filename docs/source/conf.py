de1afd2646753114fbfc48c6b113134250dd9e1a
AtrCheema <ather_abbas786@yahoo.com>
2022-03-02 15:52:45 +0900

    added sphinx-gallry

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
 'sphinx_toggleprompt',
 'sphinx_copybutton',
 "sphinx-prompt",
+"sphinx_gallery.gen_gallery",
 ]
 
 toggleprompt_offset_right  = 30
@@ -56,6 +57,33 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+sphinx_gallery_conf = {
+    'backreferences_dir': 'gen_modules/backreferences',
+    #'doc_module': ('sphinx_gallery', 'numpy'),
+    'reference_url': {
+        'sphinx_gallery': None,
+    },
+    'examples_dirs': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'examples'),
+    'gallery_dirs': 'auto_examples',
+    'compress_images': ('images', 'thumbnails'),
+    #'filename_pattern': '',
+
+    'binder': {'org': 'sphinx-gallery',
+               'repo': 'sphinx-gallery.github.io',
+               'branch': 'master',
+               'binderhub_url': 'https://mybinder.org',
+               'dependencies': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.binder', 'requirements.txt'),
+               'notebooks_dir': 'notebooks',
+               'use_jupyter_lab': True,
+               },
+    'show_memory': True,
+    #'junit': os.path.join('sphinx-gallery', 'junit-results.xml'),
+    # capture raw HTML or, if not present, __repr__ of last expression in
+    # each code block
+    'capture_repr': ('_repr_html_', '__repr__'),
+    'matplotlib_animations': True,
+    'image_srcset': ["2x"]
+}
 
 # -- Options for HTML output -------------------------------------------------
 
